4908a5fa7e86b643c266bb07874a124d48f1f232
AlexanderVaquero <alexander.vaquero@maxikash.mx>
2025-11-03 15:03:31 -0600

    first commit

---
+++ src/main/java/com/emmdev/kafka_demo_initial/KafkaDemoInitialApplication.java
@@ -0,0 +1,13 @@
+package com.emmdev.kafka_demo_initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoInitialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoInitialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmdev/kafka_demo_initial/KafkaDemoInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmdev.kafka_demo_initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
